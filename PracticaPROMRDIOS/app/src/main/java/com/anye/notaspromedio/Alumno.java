604a2ffd03ef9525de65d73d6aad2889b7c2c24b
anyiecon <88735788+anyiecon@users.noreply.github.com>
2022-04-01 00:19:34 -0500

    Delete PracticaPROMRDIOS directory

--- PracticaPROMRDIOS/app/src/main/java/com/anye/notaspromedio/Alumno.java
+++
@@ -1,9 +0,0 @@
-package com.anye.notaspromedio;
-
-public class Alumno {
-    public String documento;
-    public String nombre;
-    public int edad;
-    public int telefono;
-    public String dirección ;
-}
